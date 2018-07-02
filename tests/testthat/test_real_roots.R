040d382a97dca6e800205b4d1964914f63a33942
Dev Chakraborty <dpc10ster@gmail.com>
2018-07-01 20:38:41 -0400

    testthat
    still learning this part; no useful tests yet

--- tests/testthat/test_real_roots.R
+++ tests/testthat/test_real_roots.R
@@ -4,46 +4,45 @@ test_that("Distinct roots", {
 
   roots <- real.roots(1, 7, 12)
 
-  expect_that( roots, is_a("numeric") )
-  expect_that( length(roots), equals(2) )
-  expect_that( roots[1] < roots[2], is_true() )
+  expect_true( is.numeric(roots) )
+  expect_lt( roots[1] , roots[2])
 })
 
 test_that("Repeated root", {
 
   roots <- real.roots(1, 6000, 9000000)
 
-  expect_that( length(roots), equals(1) )
+  expect_equal( length(roots), 1)
 
-  expect_that( roots, equals(-3000) )
+  expect_equal( roots, -3000)
 
   # Test whether ABSOLUTE error is within 0.1
-  expect_that( roots, equals(-3000.01, tolerance  = 0.1) )
+  expect_equal( roots, -3100.01, tolerance  = 0.1)
 
   # Test whether RELATIVE error is within 0.1
   # To test relative error, set 'scale' equal to expected value.
   # See base R function all.equal for optional argument documentation.
-  expect_equal( roots, -3001, tolerance  = 0.1, scale=-3001)
+  expect_equal( roots, -3901, tolerance  = 0.1, scale=-3000)
 })
 
 test_that("Polynomial must be quadratic", {
 
   # Test for ANY error
-  expect_that( real.roots(0, 2, 3), throws_error() )
+  expect_error( real.roots(0, 2, 3))
 
   # Test specifically for an error string containing "zero"
-  expect_that( real.roots(0, 2, 3), throws_error("zero") )
+  expect_error( real.roots(0, 2, 3), "zero" )
 
   # Test specifically for an error string containing "zero" or "Zero" using regular expression
-  expect_that( real.roots(0, 2, 3), throws_error("[zZ]ero") )
+  expect_error( real.roots(0, 2, 3), "[zZ]ero" )
 })
 
 
 
 # test_that("Bogus tests", {
-#   
+# 
 #   x <- c(1, 2, 3)
-#   
+# 
 #   expect_that( length(x), equals(2.7) )
 #   expect_that( x, is_a("data.frame") )
 # })
