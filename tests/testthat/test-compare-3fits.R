d20d5f929c630585ef44294b5aa0468c0791cb9b
Dev Chakraborty <dpc10ster@gmail.com>
2019-06-28 15:58:54 -0400

    seed = 1
    A long shot; couls still fail on Peter's system; if so will switch method of comparing output to known good values, allowing for very small differences;

--- tests/testthat/test-compare-3fits.R
+++ tests/testthat/test-compare-3fits.R
@@ -3,22 +3,27 @@ test_that("Compare3ProperRocFits", {
   skip_on_cran()
   skip_on_travis()
   
-  fn <- paste0(test_path(), '/tempValues/Compare3ProperRocFits01')
+  set.seed(1)
+  fn <- paste0(test_path(), '/goodValues/Compare3ProperRocFits01')
   if (!file.exists(fn)) {
     expect_known_output(Compare3ProperRocFits(1,1,reAnalyze = TRUE),
                         fn, print = TRUE, update = TRUE)
   }
+  
+  set.seed(1)
   expect_known_output(
     Compare3ProperRocFits(1,1,reAnalyze = TRUE),
     fn, print = TRUE, update = FALSE)
   
-  fn <- paste0(test_path(), '/tempValues/Compare3ProperRocFits02')
+  set.seed(1)
+  fn <- paste0(test_path(), '/goodValues/Compare3ProperRocFits02')
   if (!file.exists(fn)) {
     expect_known_output(
       Compare3ProperRocFits(3,3,reAnalyze = TRUE, showPlot = TRUE),
       fn, print = TRUE, update = TRUE)
   }
   
+  set.seed(1)
   expect_known_output(
     Compare3ProperRocFits(3,3,reAnalyze = TRUE, showPlot = TRUE),
     fn, print = TRUE, update = FALSE)
