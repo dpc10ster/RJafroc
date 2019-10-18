30e6c5e5b52df3b349253a2f5f1f4038724bd7a8
dpc10ster <dpc10ster@gmail.com>
2019-10-17 22:19:33 -0400

    Almost done
    Added all tests except test-OutputReport

---
+++ tests/testthat/test-plotting.R
@@ -0,0 +1,140 @@
+context("Plotting routines")
+
+test_that("PlotBinormalFit", {
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/Binormal", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotBinormalFit(c(1, 2), c(0.5, 0.5))
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotBinormalFit(c(1, 2), c(0.5, 0.5)), ret)
+  # end of test
+  
+})
+
+test_that("PlotCbmFit", {
+  Sys.sleep(0.2)
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/Cbm", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotCbmFit(c(1, 2), c(0.5, 0.5))
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotCbmFit(c(1, 2), c(0.5, 0.5)), ret)
+  # end of test
+  
+})
+
+test_that("PlotRsmOperatingCharacteristics", {
+  Sys.sleep(0.2)
+  
+  lesDistr <- rbind(c(1, 0.2), c(2, 0.4), c(3, 0.1), c(4, 0.3))
+  lesionWeights <- rbind(c(1.0, -Inf, -Inf, -Inf), 
+                         c(0.4,  0.6, -Inf, -Inf), 
+                         c(0.2,  0.3,  0.5, -Inf), 
+                         c(0.3,  0.4, 0.2,  0.1))
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/Rsm", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotRsmOperatingCharacteristics(mu = c(2, 3), lambda = c(1, 1.5), nu = c(0.6, 0.8),
+                                           lesDistr = lesDistr, lesionWeights = lesionWeights, 
+                                           legendPosition = "bottom", nlfRange = c(0, 1), llfRange = c(0, 1))
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotRsmOperatingCharacteristics(mu = c(2, 3), lambda = c(1, 1.5), nu = c(0.6, 0.8),
+                                               lesDistr = lesDistr, lesionWeights = lesionWeights, 
+                                               legendPosition = "bottom", nlfRange = c(0, 1), llfRange = c(0, 1)), ret)
+  # end of test
+  
+})
+
+test_that("PlotOperatingCharacteristics", {
+  Sys.sleep(0.2)
+  
+  plotT <- list(1, 2, c(1:2), c(1:2))
+  plotR <- list(2, c(2:3), c(1:3), 1)
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/ds04-ROC", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR) # ROC
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR), ret)
+  # end of test
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/ds04-FROC", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "FROC") # FROC
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "FROC"), ret)
+  # end of test
+  
+  Sys.sleep(0.2)
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/ds04-AFROC", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "AFROC") # AFROC
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "AFROC"), ret)
+  # end of test
+  
+  Sys.sleep(0.2)
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/ds04-wAFROC", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "wAFROC") # AFROC
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "wAFROC"), ret)
+  # end of test
+  
+  Sys.sleep(0.2)
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/ds04-AFROC1", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "AFROC1") # AFROC1
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "AFROC1"), ret)
+  # end of test
+  
+  Sys.sleep(0.2)
+  
+  fn <- paste0(test_path(), "/goodValues361/Plots/ds04-wAFROC1", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "wAFROC1") # wAFROC1
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(PlotEmpiricalOperatingCharacteristics(dataset04, trts = plotT, rdrs = plotR, opChType = "wAFROC1"), ret)
+  # end of test
+  
+})
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,164 @@
+test_that("UtilIntrinsic2Physical", {
+  mu <- 2;lambda <- 20;nu <- 1.1512925
+
+  fn <- paste0(test_path(), "/goodValues361/Utils/Intrinsic2PhysicalRSM", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- UtilIntrinsic2PhysicalRSM(mu, lambda, nu)
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  expect_equal(UtilIntrinsic2PhysicalRSM(mu, lambda, nu), ret)
+  # end of test
+
+})
+
+
+
+test_that("UtilAucBinormal", {
+  a <- 2;b <- 0.7
+
+  fn <- paste0(test_path(), "/goodValues361/Utils/AucBinormal", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- UtilAucBinormal(a,b)
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  expect_equal(UtilAucBinormal(a,b), ret)
+  # end of test
+
+})
+
+
+
+test_that("UtilAucCBM", {
+  mu <- 2;alpha <- 0.8
+
+  fn <- paste0(test_path(), "/goodValues361/Utils/AucCbm", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- UtilAucCBM(mu,alpha)
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  expect_equal(UtilAucCBM(mu,alpha), ret)
+  # end of test
+
+})
+
+
+
+test_that("UtilAucPROPROC", {
+  c1 <- .2;da <- 1.5
+
+  fn <- paste0(test_path(), "/goodValues361/Utils/AucPROPROC", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- UtilAucPROPROC(c1,da)
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  expect_equal(UtilAucPROPROC(c1,da), ret)
+  # end of test
+
+})
+
+
+
+test_that("UtilAucsRSM", {
+  mu <- 1;lambdaP <- 1;nuP <- 1
+  lesDistr <- rbind(c(1, 0.9), c(2, 0.1))
+
+    fn <- paste0(test_path(), "/goodValues361/Utils/AucRSM", ".rds")
+    if (!file.exists(fn)) {
+      warning(paste0("File not found - generating new ",fn))
+      ret <- UtilAucsRSM(mu, lambdaP, nuP, lesDistr)
+      saveRDS(ret, file = fn)
+    }
+
+    ret <- readRDS(fn)
+    expect_equal(UtilAucsRSM(mu, lambdaP, nuP, lesDistr), ret)
+    # end of test
+
+})
+
+
+
+test_that("UtilPseudoValues", {
+
+  dataset <- dataset05
+  # "Wilcoxon" will generate error, skipping "SongA1" and "SongA2"
+  FOM_arr <- c("AFROC", "AFROC1", "wAFROC", "wAFROC1", "MaxNLF", "MaxLLF", "MaxNLFAllCases",
+               "ExpTrnsfmSp", "HrSp", "HrSe")
+
+  for (i in 1:length(FOM_arr)) {
+
+    fn <- paste0(test_path(), "/goodValues361/Utils/PseudoValues", "-", FOM_arr[i], ".rds")
+    if (!file.exists(fn)) {
+      warning(paste0("File not found - generating new ",fn))
+      ret <- UtilPseudoValues(dataset, FOM = FOM_arr[i])
+      saveRDS(ret, file = fn)
+    }
+
+    ret <- readRDS(fn)
+    expect_equal(UtilPseudoValues(dataset, FOM = FOM_arr[i]), ret,
+      info = paste0("FOM = ",FOM_arr[i])
+    )
+    # end of test
+
+  }
+
+})
+
+
+
+test_that("UtilMeanSquaresDBMH", {
+
+  fn <- paste0(test_path(), "/goodValues361/Utils/PseudoValues", "-", "Wilcoxon", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- UtilPseudoValues(dataset02, FOM = "Wilcoxon")
+    saveRDS(ret, file = fn)
+  }
+
+  ret1 <- readRDS(fn)
+  ret2 <- UtilPseudoValues(dataset02, FOM = "Wilcoxon")
+  expect_equal(ret2, ret1)
+  # end of test
+
+})
+
+
+
+test_that("UtilLesionDistribution", {
+
+    fn <- paste0(test_path(), "/goodValues361/Utils/LesionWeights01", ".rds")
+    if (!file.exists(fn)) {
+      warning(paste0("File not found - generating new ",fn))
+      ret <- UtilLesionWeights (UtilLesionDistribution(dataset01))
+      saveRDS(ret, file = fn)
+    }
+
+    ret <- readRDS(fn)
+    expect_equal(UtilLesionWeights (UtilLesionDistribution(dataset01)), ret)
+    # end of test
+
+    fn <- paste0(test_path(), "/goodValues361/Utils/LesionWeights05", ".rds")
+    if (!file.exists(fn)) {
+      warning(paste0("File not found - generating new ",fn))
+      ret <- UtilLesionWeights (UtilLesionDistribution(dataset05))
+      saveRDS(ret, file = fn)
+    }
+
+    ret <- readRDS(fn)
+    expect_equal(UtilLesionWeights (UtilLesionDistribution(dataset05)), ret)
+    # end of test
+
+})
+
+
