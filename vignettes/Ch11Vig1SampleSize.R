5622de8a6e57bac5925258df6015ec7c2189fec0
dpc10ster <dpc10ster@gmail.com>
2019-12-22 18:49:04 -0500

    Added kableExtra to DESCRIPTION
    Added table capability to vignettes; see Ch11Vig1
    Updated dependencies and ran R CMD CHK
    Updated website

--- vignettes/Ch11Vig1SampleSize.R
+++ vignettes/Ch11Vig1SampleSize.R
@@ -4,19 +4,20 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 library(ggplot2)
+library(kableExtra)
 
 ## ---- fig.show='hold'----------------------------------------------------
 ndf <- 2;ddf <- 10;ncp <- c(0,2,5,10)
-fCrit <- qf(0.95, ndf,ddf)
-cat("critical value of x for rejecting NH is ", fCrit,"\n")
+alpha <- 0.05
+fCrit <- qf(1-alpha, ndf,ddf)
 x <- seq(1, 20, 0.1)
 myLabel <- c("A", "B", "C", "D")
 myLabelIndx <- 1
+pFgtFCrit <- NULL
 for (i in 1:length(ncp))
 {
   y <- df(x,ndf,ddf,ncp=ncp[i])
-  cat("ndf = ", ndf, ", ddf = ", ddf, ", ncp = ", ncp[i], ", prob > fCrit = ", 
-      1-pf(fCrit, ndf, ddf, ncp = ncp[i]), "\n")
+  pFgtFCrit <- c(pFgtFCrit, 1-pf(fCrit, ndf, ddf, ncp = ncp[i]))
 }  
 for (i in 1:length(ncp))
 {
@@ -27,20 +28,27 @@ for (i in 1:length(ncp))
     ggtitle(myLabel[myLabelIndx]);myLabelIndx <- myLabelIndx + 1
   print(curvePlot)
 }
-fCrit_2_10 <- fCrit
+fCrit_2_10 <- fCrit # convention fCrit_ndf_ddf
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
+rowNames <- LETTERS[seq(1, 4)]
+myTab <- data.frame(ndf = rep(ndf, 4), ddf = rep(ddf, 4), 
+                    fCrit = rep(fCrit, 4), ncp = ncp, 
+                    pFgtFCrit = pFgtFCrit)
+row.names(myTab) <- rowNames
+kable(myTab)
+
+## ---- fig.show='hold', echo=FALSE----------------------------------------
 ndf <- 2;ddf <- 100
-fCrit <- qf(0.95, ndf,ddf)
-cat("critical value of x for rejecting NH is ", fCrit,"\n")
+fCrit <- qf(1-alpha, ndf,ddf)
 x <- seq(1, 20, 0.1)
 myLabel <- c("E", "F", "G", "H")
 myLabelIndx <- 1
+pFgtFCrit <- NULL
 for (i in 1:length(ncp))
 {
   y <- df(x,ndf,ddf,ncp=ncp[i])
-  cat("ndf = ", ndf, ", ddf = ", ddf, ", ncp = ", ncp[i], ", prob > fCrit = ", 
-      1-pf(fCrit, ndf, ddf, ncp = ncp[i]), "\n")
+  pFgtFCrit <- c(pFgtFCrit, 1-pf(fCrit, ndf, ddf, ncp = ncp[i]))
 }  
 for (i in 1:length(ncp))
 {
@@ -51,19 +59,27 @@ for (i in 1:length(ncp))
     ggtitle(myLabel[myLabelIndx]);myLabelIndx <- myLabelIndx + 1
   print(curvePlot)
 }
-fCrit_2_100 <- fCrit
+fCrit_2_100 <- fCrit # convention fCrit_ndf_ddf
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
+rowNames <- LETTERS[seq(5, 8)]
+temp <- data.frame(ndf = rep(ndf, 4), ddf = rep(ddf, 4), 
+                    fCrit = rep(fCrit, 4), ncp = ncp, 
+                    pFgtFCrit = pFgtFCrit)
+row.names(temp) <- rowNames
+myTab <- rbind(myTab, temp)
+kable(myTab)
+
+## ---- fig.show='hold', echo=FALSE----------------------------------------
 ndf <- 1;ddf <- 100
-fCrit <- qf(0.95, ndf,ddf)
-cat("critical value of x for rejecting NH is ", fCrit,"\n")
+fCrit <- qf(1-alpha, ndf,ddf)
 x <- seq(1, 20, 0.1)
-myLabel <- c("I <- ", "J", "K", "L")
+myLabel <- c("I", "J", "K", "L")
+pFgtFCrit <- NULL
 for (i in 1:length(ncp))
 {
   y <- df(x,ndf,ddf,ncp=ncp[i])
-  cat("ndf = ", ndf, ", ddf = ", ddf, ", ncp = ", ncp[i], ", prob > fCrit = ", 
-      1-pf(fCrit, ndf, ddf, ncp = ncp[i]), "\n")
+  pFgtFCrit <- c(pFgtFCrit, 1-pf(fCrit, ndf, ddf, ncp = ncp[i]))
 }  
 myLabelIndx <- 1
 for (i in 1:length(ncp))
@@ -75,5 +91,14 @@ for (i in 1:length(ncp))
     ggtitle(myLabel[myLabelIndx]);myLabelIndx <- myLabelIndx + 1
   print(curvePlot)
 }
-fCrit_1_100 <- fCrit
+fCrit_1_100 <- fCrit # convention fCrit_ndf_ddf
+
+## ---- echo=FALSE---------------------------------------------------------
+rowNames <- LETTERS[seq(9, 12)]
+temp <- data.frame(ndf = rep(ndf, 4), ddf = rep(ddf, 4), 
+                    fCrit = rep(fCrit, 4), ncp = ncp, 
+                    pFgtFCrit = pFgtFCrit)
+row.names(temp) <- rowNames
+myTab <- rbind(myTab, temp)
+kable(myTab)
 
