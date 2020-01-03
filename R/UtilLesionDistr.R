ce64d8d08b68994ac3096b9e478556afa67ec02b
dpc10ster <dpc10ster@gmail.com>
2020-01-02 22:49:15 -0500

    Third time, [skip ci]
    using desktop app as there are too many files to add manually

---
+++ R/UtilLesionDistr.R
@@ -0,0 +1,33 @@
+#' Lesion distribution  matrix 
+#' 
+#' @description Extracts the lesion distribution matrix for a dataset.
+#' 
+#' @param dataset The supplied dataset
+#'
+#' @return The lesion distribution matrix
+#' 
+#' @details The lesion distribution matrix has Lmax rows and two columns. 
+#'    The first column contains the integers 1, 2, ..., Lmax and the second 
+#'    column contains the fraction of diseased cases with the number of lesions 
+#'    per case specified in the first column.
+#' 
+#' 
+#' @examples
+#' UtilLesionDistr (dataset01) # FROC data
+#' UtilLesionDistr (dataset02) # ROC data
+#' UtilLesionDistr (datasetCadLroc) # LROC data
+#' 
+#' @export 
+UtilLesionDistr <- function(dataset)
+{  
+  lesionNum <- dataset$lesionVector
+  lesDistr <- table(lesionNum)
+  if (length(lesDistr) == 1) {
+    lesDistr <- c(lesionNum[1], 1)
+    dim(lesDistr) <- c(1, 2)
+  }else{
+    lesDistr <- t(rbind(as.numeric(unlist(attr(lesDistr, "dimnames"))), as.vector(lesDistr)))
+  }
+  lesDistr[,2] <- lesDistr[,2]/sum(lesDistr[,2])
+  return(lesDistr)
+}
---
+++ vignettes/Ch10Vig2QuickStartDBMHExcel.R
@@ -0,0 +1,13 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+library(RJafroc)
+
+## -----------------------------------------------------------------------------
+ret <- UtilOutputReport(dataset03, FOM = "Wilcoxon", overWrite = TRUE, ReportFileExt = "xlsx")
+
+## -----------------------------------------------------------------------------
+ret <- UtilOutputReport(dataset03, FOM = "Wilcoxon", method = "ORH", overWrite = TRUE, ReportFileExt = "xlsx")
+
