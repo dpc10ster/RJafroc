4223c951610c9a00f91a8da7b48c0582517a6ab5
dpc10ster <dpc10ster@gmail.com>
2020-01-09 17:56:01 -0500

    Revert "After trimming files"
    This reverts commit 376946e81067e972c9aacbc46221ab9d7a1565ef.

---
+++ tests/testthat/test-fitting.R
@@ -0,0 +1,117 @@
+context("Fitting routines")
+
+# > devtools::test()
+# Loading RJafroc
+# Testing RJafroc
+# ✔ |  OK F W S | Context
+# ✔ |   1       | compare-3fits [6.5 s]
+# ✔ |   2       | corcbm [1.2 s]
+# ✔ |  33       | data-file [8.3 s]
+# ✔ |  33       | data-file
+# ✔ |  18       | Fitting routines [195.9 s]
+
+
+test_that("FitBinormalRoc", {
+  skip_on_travis()
+  fn <- paste0(test_path(), "/goodValues361/Fitting/BinormalRoc02", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- FitBinormalRoc(dataset02)[1:7]
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  expect_equal(FitBinormalRoc(dataset02)[1:7], ret)
+  # end of test
+
+})
+
+
+test_that("FitBinormalRoc", {
+  skip_on_travis()
+  fn <- paste0(test_path(), "/goodValues361/Fitting/BinormalRoc05", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- FitBinormalRoc(DfBinDataset(dataset05, desiredNumBins = 5, opChType = "ROC"))
+    ret <- ret[1:7]
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  ret1 <- FitBinormalRoc(DfBinDataset(dataset05, desiredNumBins = 5, opChType = "ROC"))
+  ret1 <- ret1[1:7]
+  expect_equal(ret1, ret)
+  # end of test
+
+})
+
+
+test_that("FitCbmRoc", {
+  skip_on_travis()
+  fn <- paste0(test_path(), "/goodValues361/Fitting/CbmRoc", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- FitCbmRoc(dataset02)[1:7]
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  expect_equal(FitCbmRoc(dataset02)[1:7], ret)
+  # end of test
+
+})
+
+
+test_that("FitCorCbm", {
+
+  skip_on_travis()
+  skip_on_cran()
+  skip_on_os("mac")
+  
+  fn <- paste0(test_path(), "/goodValues361/Fitting/CorCbm", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- FitCorCbm(DfExtractCorCbmDataset(dataset05, trts = 1, rdrs = c(4,7)))
+    ret <- ret$fitCorCbmRet
+    ret <- ret[1:10] # leave out covariance matrix and plots
+    saveRDS(ret, file = fn)
+  }
+
+  ret <- readRDS(fn)
+  ret1 <- FitCorCbm(DfExtractCorCbmDataset(dataset05, trts = 1, rdrs = c(4,7)))
+  ret1 <- ret1$fitCorCbmRet
+  ret1 <- ret1[1:10] # leave out covariance matrix and plots
+  expect_equal(ret1, ret)
+  # end of test
+
+})
+
+
+test_that("FitRsmRoc", {
+  
+  skip_on_cran()
+  skip_on_travis()
+  
+  fn <- paste0(test_path(), "/goodValues361/Fitting/RsmRoc", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- FitRsmRoc(dataset02, UtilLesionDistr(dataset02))[1:8]
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(FitRsmRoc(dataset02, UtilLesionDistr(dataset02))[1:8], ret, tolerance = 1e-6)
+  # end of test
+
+  fn <- paste0(test_path(), "/goodValues361/Fitting/RsmRocDegenerate", ".rds")
+  if (!file.exists(fn)) {
+    warning(paste0("File not found - generating new ",fn))
+    ret <- FitRsmRoc(datasetDegenerate, UtilLesionDistr(datasetDegenerate))[1:8]
+    saveRDS(ret, file = fn)
+  }
+  
+  ret <- readRDS(fn)
+  expect_equal(FitRsmRoc(datasetDegenerate, UtilLesionDistr(datasetDegenerate))[1:8], ret, tolerance = 1e-6)
+  # end of test
+  
+})
---
+++ vignettes/Ch30Vig1RoiParadigm.R
@@ -0,0 +1,22 @@
+## ----setup, include = FALSE----------------------------------------------
+  knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+  )
+  library(RJafroc)
+
+## ------------------------------------------------------------------------
+str(datasetROI)
+datasetROI$NL[1,1,1,]
+mean(datasetROI$NL[,,1:50,])
+datasetROI$NL[1,1,51,]
+datasetROI$lesionVector[1]
+datasetROI$LL[1,1,1,]
+x <- datasetROI$LL;mean(x[is.finite(x)])
+
+## ------------------------------------------------------------------------
+## fileName <- system.file(
+##     "extdata", "RoiData.xlsx", package = "RJafroc", mustWork = TRUE)
+## ds <- DfReadDataFile(fileName)
+## ds$dataType
+
