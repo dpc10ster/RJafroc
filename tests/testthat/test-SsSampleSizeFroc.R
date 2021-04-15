edc054d88597dbf365034fada59e094d65648f2c
dpc10ster <dpc10ster@gmail.com>
2021-04-14 21:52:03 -0400

    Update test-SsSampleSizeFroc.R
    Peter's fix to Issue 70

--- tests/testthat/test-SsSampleSizeFroc.R
+++ tests/testthat/test-SsSampleSizeFroc.R
@@ -2,7 +2,7 @@ contextStr <- "Sample Size FROC"
 context(contextStr)
 test_that(contextStr, {
  
-  skip_on_os("windows") 
+  # skip_on_os("windows") 
   skip_on_os("linux") 
   skip_on_os("solaris") 
   
@@ -18,8 +18,16 @@ test_that(contextStr, {
   
   x1 <- readRDS(fn)
   x2 <-SsFrocNhRsmModel(frocNhData, lesDistr = lesDistr)
-  expect_equal(x1,x2)
-
+  ## expect_equal(x1,x2) # this fails on other operating systems
+  # see Issue #70
+  # as per Peter's suggestions
+  expect_identical(names(x1), names(x2))
+  expect_equivalent(x1$muMed, x2$muMed, tolerance=5e-4)
+  expect_equivalent(x1$lambdaMed, x2$lambdaMed, tolerance=5e-5)
+  expect_equivalent(x1$nuMed, x2$nuMed, tolerance=5e-5)
+  expect_equivalent(x1$ScaleFactor, x2$ScaleFactor, tolerance=5e-5)
+  expect_equivalent(x1$R2, x2$R2, tolerance=1e-7)
+  
 })
 
 
