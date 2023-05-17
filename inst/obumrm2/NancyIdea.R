92049734222c9074e9663a1a823a3e43f42f758b
dpc10ster <dpc10ster@gmail.com>
2023-05-17 15:41:54 -0400

    Update NancyIdea.R

--- inst/obumrm2/NancyIdea.R
+++ inst/obumrm2/NancyIdea.R
@@ -4,13 +4,16 @@ library(RJafroc)
 
 source("inst/obumrm2/covariance.R")
 
-fileName1 <- system.file(
-  "extdata", "toyFiles/roc/roc1.xlsx", package = "RJafroc", mustWork = TRUE)
-fileName2 <- system.file(
-  "extdata", "toyFiles/roc/roc2.xlsx", package = "RJafroc", mustWork = TRUE)
+# fileName1 <- system.file(
+#   "extdata", "toyFiles/roc/roc1.xlsx", package = "RJafroc", mustWork = TRUE)
+# fileName2 <- system.file(
+#   "extdata", "toyFiles/roc/roc2.xlsx", package = "RJafroc", mustWork = TRUE)
+# 
+# ds1 <- DfReadDataFile(fileName1, newExcelFileFormat = T)
+# ds2 <- DfReadDataFile(fileName2, newExcelFileFormat = T)
 
-ds1 <- DfReadDataFile(fileName1, newExcelFileFormat = T)
-ds2 <- DfReadDataFile(fileName2, newExcelFileFormat = T)
+ds1 <- dataset02
+ds2 <- dataset03
 
 FOM <- "Wilcoxon"
 
