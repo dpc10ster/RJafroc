e597d26be159d4e93ebd5fb01aeb62d2efd03b63
dpc10ster <dpc10ster@gmail.com>
2024-04-18 21:08:42 -0400

    Update fixSP.R

--- inst/fixSP/fixSP.R
+++ inst/fixSP/fixSP.R
@@ -1,5 +1,6 @@
 library(RJafroc)
-fileName <- system.file("extdata", "/toyFiles/ROC/rocSpA-unbalanced.xlsx",
+#fileName <- system.file("extdata", "/toyFiles/ROC/rocSpA-unbalanced.xlsx",
+fileName <- system.file("extdata", "/toyFiles/ROC/rocSpA.xlsx",
                         package = "RJafroc", mustWork = TRUE)
 dsSpA <- DfReadSP(fileName)
 ret <- StSP(dsSpA, FOM = "Wilcoxon")
